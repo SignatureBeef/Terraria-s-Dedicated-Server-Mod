a595cbb299798092a7279132a9ab7426957575f5
DeathCradle <DeathCradle@live.com.au>
2011-06-18 21:57:55 +1000

    Added more features to Plugin Example & Renames Properties to ServerProperties

---
+++ TDSMPlugin/TDSMPlugin/Properties.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Terraria_Server;
+
+namespace TDSMPlugin
+{
+    public class Properties : PropertiesFile
+    {
+        public Properties(String pFile)
+        {
+            base.setFile(pFile);
+        }
+
+        public void pushData()
+        {
+            setSpawningCancelled(isSpawningCancelled());
+            setTileBreakage(getTileBreakage());
+        }
+
+        public bool isSpawningCancelled()
+        {
+            string AllowSpawns = base.getValue("allowspawns");
+            if (AllowSpawns == null || AllowSpawns.Trim().Length < 0)
+            {
+                return true;
+            }
+            else
+            {
+                return Boolean.Parse(AllowSpawns);
+            }
+        }
+
+        public void setSpawningCancelled(bool AllowSpawns)
+        {
+            base.setValue("allowspawns", AllowSpawns.ToString());
+        }
+
+        public bool getTileBreakage()
+        {
+            string TileBreakage = base.getValue("tilebreakage");
+            if (TileBreakage == null || TileBreakage.Trim().Length < 0)
+            {
+                return true;
+            }
+            else
+            {
+                return Boolean.Parse(TileBreakage);
+            }
+        }
+
+        public void setTileBreakage(bool TileBreakage)
+        {
+            base.setValue("tilebreakage", TileBreakage.ToString());
+        }
+    }
+}
