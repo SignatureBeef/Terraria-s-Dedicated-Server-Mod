d1ebedc6f2d97b69ebae21b7727f1d0062497380
DeathCradle <DeathCradle@live.com.au>
2011-06-26 00:19:32 +1000

    Added Forgotten classes...Again ;D

---
+++ Terraria_Server/Events/PlayerDeathEvent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Terraria_Server.Events
+{
+    public class PlayerDeathEvent : Event
+    {
+        string deathMessage = "";
+
+        public string getDeathMessage()
+        {
+            return deathMessage;
+        }
+
+        public void setDeathMessage(string DeathMessage)
+        {
+            deathMessage = DeathMessage;
+        }
+
+        public Player getPlayer()
+        {
+            return (Player)base.getSender();
+        }
+    }
+}
