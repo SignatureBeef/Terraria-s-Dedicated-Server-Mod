bbf161f0073a332857e334be153ef90c984d5c3c
DeathCradle <DeathCradle@live.com.au>
2011-07-04 21:11:41 +1000

    Added NPCDeathEvent like i forgot =D

---
+++ Terraria_Server/Events/NPCDeathEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Terraria_Server.Events
+{
+    public class NPCDeathEvent : Event
+    {
+        public NPC Npc { get; set; }
+        public int Damage { get; set; }
+        public float KnockBack { get; set; }
+        public int HitDirection { get; set; }
+    }
+}
