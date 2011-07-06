125dbabe3a732396e1024f00a9d008fd4772202f
DeathCradle <DeathCradle@live.com.au>
2011-07-07 07:36:39 +1000

    Added new Files

---
+++ Terraria_Server/Events/NPCSpawnEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Terraria_Server.Events
+{
+    public class NPCSpawnEvent : Event
+    {
+        public NPC NPC { get; set; }
+    }
+}
---
+++ Terraria_Server/Events/PlayerMoveEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Terraria_Server.Misc;
+
+namespace Terraria_Server.Events
+{
+    public class PlayerMoveEvent : BasePlayerEvent
+    {
+        public Vector2 Location { get; set; }
+        public Vector2 Velocity { get; set; }
+        public int FallStart { get; set; }
+    }
+}
