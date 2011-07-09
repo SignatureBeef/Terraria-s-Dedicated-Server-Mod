f11e37cfc2717b55952b747835c54f3b94a056e8
DeathCradle <DeathCradle@live.com.au>
2011-07-09 14:10:54 +1000

    Arranged Files for ease of access later for us & dev's

---
+++ Terraria_Server/Definitions/DoorOpener.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Terraria_Server.Plugin
+{
+    /// <summary>
+    /// Enumerates whether a door was opened by a player, NPC, the server, or some unknown entity
+    /// </summary>
+    public enum DoorOpener
+    {
+        PLAYER,
+        NPC,
+        SERVER,
+        UNKNOWN
+    }
+}
