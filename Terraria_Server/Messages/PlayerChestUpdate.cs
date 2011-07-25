2ed92d0277bc50fa6136ca9cc78779c4113147df
DeathCradle <DeathCradle@live.com.au>
2011-07-25 12:30:03 +1000

    Checked over packet's to what they should apparently read

--- Terraria_Server/Messages/PlayerChestUpdate.cs
+++ Terraria_Server/Messages/PlayerChestUpdate.cs
@@ -11,7 +11,7 @@ namespace Terraria_Server.Messages
 
         public void Process(int start, int length, int num, int whoAmI, byte[] readBuffer, byte bufferData)
         {
-            int inventoryIndex = BitConverter.ToInt32(readBuffer, num);
+            int inventoryIndex = (int)BitConverter.ToInt16(readBuffer, num);
             num += 2;
             int x = BitConverter.ToInt32(readBuffer, num);
             num += 4;
