e9c40c8625767645d1513923ad41626647c5e943
Malkierian <rhydonj@gmail.com>
2011-07-27 12:17:20 -0700

    Add a lot of documentation to Item.cs.

--- Terraria_Server/Item.cs
+++ Terraria_Server/Item.cs
@@ -7,13 +7,24 @@ using Terraria_Server.WorldMod;
 
 namespace Terraria_Server
 {
+	/// <summary>
+	/// Items
+	/// </summary>
     public class Item : BaseEntity
     {
+		/// <summary>
+		/// Potion delay time
+		/// </summary>
         public const int POTION_DELAY = 720;
-
+		/// <summary>
+		/// ProjectileType of ammo used when Item used.
+		/// </summary>
         public ProjectileType Ammo;
         public bool AutoReuse;
         public int Axe;
+		/// <summary>
+		/// Whether the item is currently being grabbed
+		/// </summary>
         public bool BeingGrabbed;
         public int BodySlot = -1;
         public int BuffTime;
@@ -24,39 +35,90 @@ namespace Terraria_Server
         public int CreateTile;
         public int CreateWall;
         public int Damage;
+		/// <summary>
+		/// Defense added by this item
+		/// </summary>
         public int Defense;
         public int Hammer;
         public int HeadSlot;
+		/// <summary>
+		/// Amount of life restoration provided by this item
+		/// </summary>
         public int HealLife;
+		/// <summary>
+		/// Amount of mana restoration provided by this item
+		/// </summary>
         public int HealMana;
         public int HoldStyle;
         public int KeepTime;
+		/// <summary>
+		/// Knockback provided by this item
+		/// </summary>
         public float KnockBack;
         public bool LavaWet;
         public int LegSlot;
+		/// <summary>
+		/// Amount of life regeneration provided by this item
+		/// </summary>
         public int LifeRegen;
+		/// <summary>
+		/// Mana required to use this item
+		/// </summary>
         public int Mana;
+		/// <summary>
+		/// Amount of life regeneration provided by this item
+		/// </summary>
         public int ManaRegen;
+		/// <summary>
+		/// Maximum number of this item that can be in one stack
+		/// </summary>
         public int MaxStack;
+		/// <summary>
+		/// Whether this is a melee item or not
+		/// </summary>
         public bool Melee;
         public int NoGrabDelay;
         public bool NoMelee;
+		/// <summary>
+		/// Whether this item can be wet or not
+		/// </summary>
         public bool NoWet;
+		/// <summary>
+		/// Player index value of item's owner
+		/// </summary>
         public int Owner = 255;
+		/// <summary>
+		/// Time left to ignore pickup
+		/// </summary>
         public int OwnIgnore = -1;
         public int OwnTime;
         public int Pick;
         public int PlaceStyle;
+		/// <summary>
+		/// Whether item is a potion
+		/// </summary>
         public bool Potion;
         public int Rare;
         public int Release;
         public float Scale;
+		/// <summary>
+		/// Projectile this item shoots
+		/// </summary>
         public ProjectileType Shoot;
+		/// <summary>
+		/// Speed at which this item shoots projectiles
+		/// </summary>
         public float ShootSpeed;
         public int SpawnTime;
+		/// <summary>
+		/// Current item stack amount
+		/// </summary>
         public int Stack;
         public int TileBoost;
         public String ToolTip;
+		/// <summary>
+		/// Essentially the same as Ammo
+		/// </summary>
         public ProjectileType UseAmmo;
         public int UseAnimation;
         public int UseStyle;
@@ -64,9 +126,15 @@ namespace Terraria_Server
         public bool UseTurn;
         public int Value;
         public Vector2 Velocity;
+		/// <summary>
+		/// Whether the item is currently wet
+		/// </summary>
         public bool Wet;
         public byte WetCount;
 
+		/// <summary>
+		/// Item class contstructor
+		/// </summary>
         public Item()
         {
             BodySlot = -1;
@@ -83,6 +151,12 @@ namespace Terraria_Server
             UseTime = 100;
         }
 
+		/// <summary>
+		/// Finds the names of the cobalt armor based on the current release version of Terraria
+		/// </summary>
+		/// <param name="oldName">Previous release name of item</param>
+		/// <param name="release">Release version</param>
+		/// <returns>Currently used name for item</returns>
         public static String VersionName(String oldName, int release)
         {
             if (release <= 4)
@@ -100,6 +174,10 @@ namespace Terraria_Server
             return oldName;
         }
 
+		/// <summary>
+		/// Updates specified item's condition
+		/// </summary>
+		/// <param name="i">Item index</param>
         public void UpdateItem(int i)
         {
             if (this.Active)
@@ -247,6 +325,17 @@ namespace Terraria_Server
             }
         }
 
+		/// <summary>
+		/// Creates new item
+		/// </summary>
+		/// <param name="X">X coordinate of new item location</param>
+		/// <param name="Y">Y coordinate of new item location</param>
+		/// <param name="Width">Width of new item</param>
+		/// <param name="Height">Height of new item</param>
+		/// <param name="type">New item type</param>
+		/// <param name="stack">How big of a stack to create</param>
+		/// <param name="noBroadcast">Whether to broadcast item creation or not</param>
+		/// <returns>New item index value</returns>
         public static int NewItem(int X, int Y, int Width, int Height, int type, int stack = 1, bool noBroadcast = false)
         {
             if (WorldModify.gen)
@@ -298,6 +387,10 @@ namespace Terraria_Server
             return itemIndex;
         }
 
+		/// <summary>
+		/// Finds a new owner for the specified item
+		/// </summary>
+		/// <param name="whoAmI">Item index</param>
         public void FindOwner(int whoAmI)
         {
             if (this.KeepTime > 0)
@@ -332,11 +425,20 @@ namespace Terraria_Server
             }
         }
 
+		/// <summary>
+		/// Clones the item
+		/// </summary>
+		/// <returns>Cloned object</returns>
         public override object Clone()
         {
             return base.MemberwiseClone();
         }
 
+		/// <summary>
+		/// Finds out if current item is the same as another kind of item
+		/// </summary>
+		/// <param name="compareItem">Item to compare this item to</param>
+		/// <returns>True if the same, false if not</returns>
         public bool IsTheSameAs(Item compareItem)
         {
             return this.Name == compareItem.Name;
