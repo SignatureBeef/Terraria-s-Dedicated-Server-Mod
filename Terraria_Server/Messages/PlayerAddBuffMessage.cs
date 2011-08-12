275aa861f696e469f580a2bb1043cb1c42d8eb8e
UndeadMiner <undeadminer@operamail.com>
2011-08-12 15:16:18 +0200

    Le sigh, another network message that lets clients dictate what happens.

--- Terraria_Server/Messages/PlayerAddBuffMessage.cs
+++ Terraria_Server/Messages/PlayerAddBuffMessage.cs
@@ -1,4 +1,5 @@
 using System;
+using Terraria_Server.Logging;
 
 namespace Terraria_Server.Messages
 {
@@ -12,15 +13,16 @@ namespace Terraria_Server.Messages
 		public void Process(int start, int length, int num, int whoAmI, byte[] readBuffer, byte bufferData)
 		{
 			var playerId = readBuffer[num++];
+			var type = readBuffer[num++];
+			var time = BitConverter.ToInt16 (readBuffer, num);
 			
-			if (playerId != whoAmI)
+			if (type > 26 || time > 900 /*max buff time*/
+				|| (playerId != whoAmI && ((type != 20 && type != 24) || time > 600 /*max debuff time*/)))
 			{
+				ProgramLog.Debug.Log ("PLAYER_ADD_BUFF: from={0}, for={1}, type={2}, time={3}", whoAmI, playerId, type, time);
 				Netplay.slots[whoAmI].Kick ("Cheating detected (PLAYER_ADD_BUFF forgery).");
 				return;
 			}
-			
-			var type = readBuffer[num++];
-			var time = BitConverter.ToInt16 (readBuffer, num);
 			
 			Main.players[whoAmI].AddBuff (type, time, true);
 			
