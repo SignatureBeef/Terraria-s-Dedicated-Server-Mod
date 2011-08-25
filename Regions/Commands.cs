67498b6ce23174a25f0d721b9df0c81145800028
DeathCradle <DeathCradle@live.com.au>
2011-08-26 01:42:14 +1000

    nearly functioning regions

--- Regions/Commands.cs
+++ Regions/Commands.cs
@@ -14,7 +14,13 @@ namespace Regions
         public static void Region(Server server, ISender sender, ArgumentList args)
         {
             if (args.TryPop("select"))
+            {
                 SelectionToolToggle(server, sender, args);
+            }
+            else if (args.TryPop("create"))
+            {
+                Create(server, sender, args);
+            }
         }
 
         public static void SelectionToolToggle(Server server, ISender sender, ArgumentList args)
@@ -47,7 +53,49 @@ namespace Regions
 
         public static void Create(Server server, ISender sender, ArgumentList args)
         {
+            if (sender is Player)
+            {
+                String Name = "";
+                String Desc = "";
+                Boolean Restrict = false;
+                
+                if (args.TryParseThree<String, String, Boolean>("-name", out Name, "-desc", out Desc, "-res", out Restrict)
+                    && Name.Trim().Length > 0)
+                {
+                    var player = sender as Player;
+                    if (Selection.isInSelectionlist(player))
+                    {
+                        Vector2[] regionAxis = Selection.GetSelection(player);
 
+                        Region.Region rgn = new Region.Region();
+                        rgn.Name = Name;
+                        rgn.Description = Desc;
+                        rgn.Point1 = regionAxis[0];
+                        rgn.Point2 = regionAxis[1];
+
+                        if (rgn.IsValidRegion())
+                        {
+                            Regions.regionManager.Regions.Add(rgn);
+                            if(Regions.regionManager.SaveRegion(rgn))
+                                player.sendMessage("Region '" + Name + "' was successfully created.", ChatColour.Green);
+                            else
+                                player.sendMessage("There was an issue while saving the region", ChatColour.Red);
+                        }
+                        else
+                        {
+                            player.sendMessage("There was an issue while creating the region", ChatColour.Red);
+                        }
+                    }
+                    else
+                    {
+                        player.sendMessage("You need to select a region first!", ChatColour.Red);
+                    }
+                }
+                else
+                {
+                    throw new CommandError("You have not specified certain arguments");
+                }
+            }
         }
     }
 }
