9678065c400e2ec8f31418a388e817a4fabc93d1
DeathCradle <DeathCradle@live.com.au>
2011-08-25 20:16:37 +1000

    Finished main setup for region selection

--- Regions/Regions.cs
+++ Regions/Regions.cs
@@ -5,11 +5,20 @@ using System.Text;
 using Terraria_Server.Plugin;
 using Terraria_Server.Commands;
 using Terraria_Server.Logging;
+using Terraria_Server.Events;
+using Terraria_Server.Misc;
+using Terraria_Server.Definitions.Tile;
+using Terraria_Server;
 
 namespace Regions
 {
     public class Regions : Plugin
     {
+        public const Int32 SelectorItem = 0;
+
+        public static Vector2[] mousePoints = { new Vector2(0, 0), new Vector2(0, 0) };
+        private static Boolean SelectorPos = true; //false for 1st (mousePoints[0]), true for 2nd
+
         public override void Load()
         {
             base.Name = "Regions";
@@ -23,6 +32,9 @@ namespace Regions
                 .WithHelpText("Usage:    region select")
                 .WithDescription("Turn on the Selectin Tool Mode")
                 .Calls(Commands.SelectionToolToggle);
+
+            //registerHook(Hooks.PLAYER_KEYPRESS); 
+            registerHook(Hooks.PLAYER_TILECHANGE);
         }
 
         public override void Enable()
@@ -34,5 +46,35 @@ namespace Regions
         {
             ProgramLog.Log("Regions disabled.");
         }
+        
+        #region Events
+
+            //public override void onPlayerKeyPress(PlayerKeyPressEvent Event)
+            //{
+            //    if(Event.MouseClicked)
+            //        Event.
+            //}
+
+            public override void onPlayerTileChange(PlayerTileChangeEvent Event)
+            {
+                if(Event.Sender is Player) {
+                    if (Event.Action == TileAction.PLACED && Event.TileType == TileType.BLOCK && (int)Event.Tile.Type == SelectorItem)
+                    {
+                        Event.Cancelled = true;
+                        SelectorPos = !SelectorPos;
+
+                        if (!SelectorPos)
+                            mousePoints[0] = Event.Position;
+                        else
+                            mousePoints[1] = Event.Position;
+
+                        (Event.Sender as Player).sendMessage(string.Format("You have selected block at {0},{1}, {2} position", 
+                            Event.Position.X, Event.Position.Y, (!SelectorPos) ? "First" : "Second"), ChatColour.Green);
+                    }
+                }
+            }
+
+        #endregion
+
     }
 }
