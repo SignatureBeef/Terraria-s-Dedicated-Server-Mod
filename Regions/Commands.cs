8eea75ef89eac007e910bb830a7e19ed9c76cb63
DeathCradle <DeathCradle@live.com.au>
2011-08-25 19:28:34 +1000

    Started on regions

---
+++ Regions/Commands.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Terraria_Server.Commands;
+using Terraria_Server;
+
+namespace Regions
+{
+    public class Commands
+    {
+        public static void SelectionToolToggle(Server server, ISender sender, ArgumentList args)
+        {
+
+        }
+
+        public static void Create(Server server, ISender sender, ArgumentList args)
+        {
+
+        }
+    }
+}
---
+++ Regions/Regions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Terraria_Server.Plugin;
+using Terraria_Server.Commands;
+using Terraria_Server.Logging;
+
+namespace Regions
+{
+    public class Regions : Plugin
+    {
+        public void Load()
+        {
+            base.Name = "Regions";
+            base.Description = "A region plguin for TDSM";
+            base.Author = "DeathCradle";
+            base.Version = "0.1";
+            base.TDSMBuild = 32;
+
+            AddCommand("region select")
+                .WithAccessLevel(AccessLevel.OP)
+                .WithHelpText("Usage:    region select")
+                .WithDescription("Turn on the Selectin Tool Mode")
+                .Calls(Commands.SelectionToolToggle);
+        }
+
+        public void Enable()
+        {
+            ProgramLog.Log("Regions for TDSM #" + base.TDSMBuild + " enabled.");
+        }
+
+        public void Disable()
+        {
+            ProgramLog.Log("Regions disabled.");
+        }
+    }
+}
