5f2bffe3edf9c5b0842684cf0ad4d15d83e1fd03
Malkierian <Malkierian@gmail.com>
2011-08-26 14:43:40 -0700

    Implemented very basic API support for Permissions.

---
+++ Terraria_Server/Permissions/Node.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Terraria_Server.Permissions
+{
+	class Node
+	{
+		private String stringNode;
+
+		// these are stable APIs
+		public string Path { get { return stringNode; } }
+
+		public Node FromPath(String node)
+		{
+			return new Node { stringNode = node };
+		}
+
+		public bool IsPermitted(Player player)
+		{
+			return isPermittedImpl(this, player);
+		}
+
+		// this is only for the permissions plugin to set
+		public static Func<Node, Player, bool> isPermittedImpl;
+	}
+}
