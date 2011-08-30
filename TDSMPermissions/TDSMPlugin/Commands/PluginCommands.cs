2afea1ef7f668ca1b8707f9ef3ed1548b8cc383f
Malkierian <Malkierian@gmail.com>
2011-08-29 20:06:03 -0700

    Completely forgot to add, not only the Permissions project file, but also all of the other files for Permissions...

---
+++ TDSMPermissions/TDSMPlugin/Commands/PluginCommands.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Terraria_Server;
+using Terraria_Server.Commands;
+using Terraria_Server.Misc;
+
+namespace TDSMPermissions.Commands
+{
+    public class PluginCommands
+    {
+        
+    }
+}
