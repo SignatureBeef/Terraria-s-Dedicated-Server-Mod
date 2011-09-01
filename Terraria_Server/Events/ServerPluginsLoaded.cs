751364b6cba7cdfec86ec68dd996b4dcb038bec7
DeathCradle <DeathCradle@live.com.au>
2011-09-02 08:40:19 +1000

    Added missing Event

---
+++ Terraria_Server/Events/ServerPluginsLoaded.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Terraria_Server.Plugin;
+
+namespace Terraria_Server.Events
+{
+    public class ServerPluginsLoaded : Event
+    {
+        public PluginManager PluginManager { get; set; }
+    }
+}
