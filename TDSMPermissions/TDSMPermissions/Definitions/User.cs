f4ebe2a0e58d8c33faeca765117bb2f746532010
Malkierian <Malkierian@gmail.com>
2011-09-18 15:59:43 -0700

    More merging woes... Forgot User.cs for permissions.

---
+++ TDSMPermissions/TDSMPermissions/Definitions/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Terraria_Server.Misc;
+
+namespace TDSMPermissions.Definitions
+{
+	class User
+	{
+		public List<string> hasPerm;
+		public List<string> notHasPerm;
+		public string prefix;
+		public string suffix;
+		public Color chatColor;
+		public List<string> group;
+
+		public User()
+		{
+			hasPerm = new List<string>();
+			notHasPerm = new List<string>();
+			group = new List<string>();
+			prefix = null;
+			suffix = null;
+			chatColor = ChatColor.AntiqueWhite;
+		}
+	}
+}
