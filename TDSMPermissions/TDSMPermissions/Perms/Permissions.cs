c6030b108ac23e3d7ccc3775962cd73595c52597
DeathCradle <DeathCradle@live.com.au>
2011-10-31 11:50:54 +1000

    Bit of organising to permissions

---
+++ TDSMPermissions/TDSMPermissions/Perms/Permissions.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TDSMPermissions.Definitions;
+using YaTools.Yaml;
+using System.IO;
+using Terraria_Server.Misc;
+using Terraria_Server;
+
+namespace TDSMPermissions.Perms
+{
+    public static class Permissions
+    {
+
+        public static string defaultGroup;
+
+        public static List<Group> groups = new List<Group>();
+        public static Dictionary<String, User> users = new Dictionary<String, User>();
+        public static User currentUser = new User();
+        public static Group currentGroup;
+        public static YamlScanner sc;
+        public static bool inUsers;
+        public static string currentUserName = null;
+        public static string[] nodesToAdd = 
+        {
+            "permissions.test"
+        };
+
+        public static void LoadPerms(string permissionsYML)
+        {
+            Token to;
+            TextReader re = File.OpenText(permissionsYML);
+
+            sc = new YamlScanner();
+            sc.SetSource(re);
+
+            inUsers = false;
+
+            while ((to = sc.NextToken()) != Token.EndOfStream)
+            {
+                switch (to)
+                {
+                    case Token.TextContent:
+                        {
+                            switch (sc.TokenText)
+                            {
+                                case "info":
+                                    {
+                                        ProcessInfo();
+                                        break;
+                                    }
+                                case "permissions":
+                                    {
+                                        ProcessPermissions();
+                                        break;
+                                    }
+                                case "inheritance":
+                                    {
+                                        ParseInheritance();
+                                        break;
+                                    }
+                                case "groups":
+                                    {
+                                        if (inUsers)
+                                        {
+                                            ProcessGroups();
+                                        }
+                                        break;
+                                    }
+                                case "users":
+                                    {
+                                        inUsers = true;
+                                        ProcessInheritance();
+                                        break;
+                                    }
+                                default:
+                                    break;
+                            }
+                            break;
+                        }
+                    case Token.IndentSpaces:
+                        {
+                            ProcessIndent();
+                            break;
+                        }
+                    case Token.Outdent:
+                    case Token.ValueIndicator:
+                    case Token.BlockSeqNext:
+                    case Token.Comma:
+                    case Token.Escape:
+                    case Token.InconsistentIndent:
+                    case Token.Unexpected:
+                    case Token.DoubleQuote:
+                    case Token.SingleQuote:
+                    case Token.EscapedLineBreak:
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public static void ProcessGroups()
+        {
+            while (sc.NextToken() != Token.Outdent)
+            {
+                while (sc.NextToken() != Token.TextContent)
+                {
+                    if (sc.Token == Token.Outdent)
+                        return;
+                }
+                foreach (Group group in groups)
+                {
+                    if (group.Name == sc.TokenText)
+                    {
+                        foreach (string node in group.permissions.Keys)
+                        {
+                            bool toggle = false;
+                            group.permissions.TryGetValue(node, out toggle);
+
+                            if (toggle)
+                                currentUser.hasPerm.Add(node);
+                            else
+                                currentUser.notHasPerm.Add(node);
+                        }
+                        currentUser.group.Add(group.Name);
+                        if (!users.ContainsKey(currentUserName))
+                            users.Add(currentUserName, currentUser);
+                    }
+                }
+            }
+        }
+
+        public static void ProcessIndent()
+        {
+            string tokenText = sc.TokenText;
+            if (sc.NextToken() == Token.IndentSpaces)
+                tokenText += sc.TokenText;
+
+            if (tokenText == "    ")
+            {
+                while (sc.NextToken() != Token.TextContent)
+                {
+                }
+                if (!inUsers)
+                {
+                    currentGroup = new Group(sc.TokenText);
+                }
+                else
+                {
+                    currentUserName = sc.TokenText;
+                    currentUser = new User();
+                }
+            }
+        }
+
+        public static void WaitNext(YamlScanner sc, string node)
+        {
+            while (sc.TokenText != node)
+                sc.NextToken();
+        }
+
+        public static void ProcessInfo()
+        {
+            bool Default;
+            string Prefix;
+            string Suffix;
+            Color color = default(Color);
+
+            WaitNext(sc, "default");
+
+            while (sc.NextToken() != Token.TextContent) { }
+
+            Default = Convert.ToBoolean(sc.TokenText);
+            if (Default)
+                defaultGroup = currentGroup.Name;
+
+            WaitNext(sc, "prefix");
+
+            while (sc.NextToken() != Token.TextContent) { }
+            Prefix = sc.TokenText;
+
+            WaitNext(sc, "suffix");
+
+            while (sc.NextToken() != Token.TextContent) { }
+            Suffix = sc.TokenText;
+
+            while (sc.TokenText != "color")
+            {
+                sc.NextToken();
+                if (sc.Token == Token.Outdent)
+                {
+                    color = ChatColor.White;
+                    break;
+                }
+            }
+
+            if (sc.Token == Token.TextContent && sc.TokenText == "color")
+            {
+                while (sc.NextToken() != Token.TextContent)
+                {
+                    if (sc.Token == Token.Outdent)
+                        break;
+                }
+                Color col;
+                if (sc.Token == Token.TextContent && Color.TryParseRGB(sc.TokenText, out col))
+                    color = col;
+                //else
+                //    color = ChatColor.White;
+            }
+
+            if (inUsers)
+                currentUser.SetUserInfo(Prefix, Suffix, color);
+            else
+                currentGroup.SetGroupInfo(Default, Prefix, Suffix, color);
+        }
+
+        public static void ParseInheritance()
+        {
+            while (sc.NextToken() != Token.TextContent)
+            {
+                if (sc.Token == Token.Outdent)
+                    return;
+            }
+            currentGroup.Inherits.Add(sc.TokenText);
+        }
+
+        public static void ProcessInheritance()
+        {
+            //ProgramLog.Debug.Log("Processing group inheritances");
+            foreach (Group group in groups)
+            {
+                //ProgramLog.Debug.Log("Processing " + group.Name + "'s inheritance");
+                group.Inherits.ForEach(delegate(string s)
+                {
+                    //ProgramLog.Debug.Log("Group " + group.Name + " inherits from " + s);
+                    foreach (Group groupIn in groups)
+                    {
+                        if (groupIn.Name == s)
+                        {
+                            foreach (string node in groupIn.permissions.Keys)
+                            {
+                                if (!group.permissions.ContainsKey(node))
+                                {
+                                    //ProgramLog.Debug.Log("Adding node " + node + " from " + s + " to " + group.Name);
+                                    bool toggle = false;
+                                    groupIn.permissions.TryGetValue(node, out toggle);
+                                    group.permissions.Add(node, toggle);
+                                }
+                            }
+                        }
+                    }
+                });
+            }
+        }
+
+        public static void ProcessPermissions()
+        {
+            while (sc.NextToken() != Token.Outdent)
+            {
+                while (sc.NextToken() != Token.TextContent)
+                {
+                    if (sc.Token == Token.Outdent)
+                    {
+                        if (!inUsers)
+                            groups.Add(currentGroup);
+                        else if (!users.ContainsKey(currentUserName))
+                            users.Add(currentUserName, currentUser);
+                        return;
+                    }
+                }
+
+                bool toggle;
+                string tokenText;
+
+                if (sc.TokenText.Contains("-"))
+                {
+                    toggle = false;
+                    tokenText = sc.TokenText.Substring(1, sc.TokenText.Length - 1);
+                }
+                else
+                {
+                    toggle = true;
+                    tokenText = sc.TokenText;
+                }
+
+                if (!inUsers)
+                {
+                    if (toggle)
+                    {
+                        if (tokenText == "*")
+                        {
+                            foreach (string s in Program.permissionManager.ActiveNodes)
+                                if (!currentGroup.permissions.ContainsKey(s))
+                                    currentGroup.permissions.Add(s, toggle);
+                        }
+                        else if (tokenText.Contains("*"))
+                        {
+                            string temp = tokenText.Remove(tokenText.Length - 2);
+
+                            foreach (string s in Program.permissionManager.ActiveNodes)
+                                if (s.Contains(temp))
+                                {
+                                    if (!currentGroup.permissions.ContainsKey(s))
+                                        currentGroup.permissions.Add(s, toggle);
+                                }
+                        }
+                    }
+
+                    if (!currentGroup.permissions.ContainsKey(tokenText))
+                        currentGroup.permissions.Add(tokenText, toggle);
+                }
+                else
+                {
+                    if (toggle)
+                    {
+                        if (tokenText == "*")
+                        {
+                            foreach (string s in Program.permissionManager.ActiveNodes)
+                                currentUser.hasPerm.Add(s);
+                        }
+                        else if (tokenText.Contains("*"))
+                        {
+                            string temp = tokenText.Remove(tokenText.Length - 2);
+                            foreach (string s in Program.permissionManager.ActiveNodes)
+                            {
+                                if (s.Contains(temp))
+                                    currentUser.hasPerm.Add(s);
+                            }
+                        }
+                        else
+                            currentUser.hasPerm.Add(tokenText);
+                    }
+                    else
+                        currentUser.notHasPerm.Add(tokenText);
+                }
+            }
+        }
+
+        public static Group GetGroup(string Name)
+        {
+            foreach (Group grp in groups)
+            {
+                if (grp.Name == Name)
+                    return grp;
+            }
+
+            return null;
+        }
+    }
+}
--- TDSMPermissions/TDSMPermissions/TDSMPermissions.cs
+++ TDSMPermissions/TDSMPermissions/TDSMPermissions.cs
@@ -12,6 +12,7 @@ using Terraria_Server.Commands;
 using YaTools.Yaml;
 using Terraria_Server.Plugins;
 using TDSMPermissions.Auth;
+using TDSMPermissions.Perms;
 
 namespace TDSMPermissions
 {
@@ -20,24 +21,6 @@ namespace TDSMPermissions
         public Properties properties;
         public string pluginFolder;
         public string permissionsYML;
-
-        public bool spawningAllowed = false;
-        public bool tileBreakageAllowed = false;
-        public bool explosivesAllowed = false;
-        public TDSMPermissions plugin;
-        public string defaultGroup;
-
-        private List<Group> groups = new List<Group>();
-        private Dictionary<String, User> users = new Dictionary<String, User>();
-        private User currentUser = new User();
-        private Group currentGroup;
-        private YamlScanner sc;
-        private bool inUsers;
-        private String currentUserName = null;
-		private string[] nodesToAdd = 
-        {
-            "permissions.test"
-        };
 
         public TDSMPermissions()
         {
@@ -50,8 +33,6 @@ namespace TDSMPermissions
 
         protected override void Initialized(object state)
         {
-            plugin = this;
-
             pluginFolder = Statics.PluginPath + Path.DirectorySeparatorChar + "TDSMPermissions";
             permissionsYML = pluginFolder + Path.DirectorySeparatorChar + "permissions.yml";
 
@@ -76,7 +57,7 @@ namespace TDSMPermissions
 				.WithDescription("Test command")
 				.Calls(Commands.PluginCommands.PermissionsCommand);
 
-			Program.permissionManager.AddNodes(nodesToAdd);
+            Program.permissionManager.AddNodes(Permissions.nodesToAdd);
         }
 
         protected override void Disabled()
@@ -104,14 +85,13 @@ namespace TDSMPermissions
             if (ctx.Player.AuthenticatedAs != null)
             {
                 User usr;
-
-                if (users.TryGetValue(ctx.Player.Name, out usr))
+                if (Permissions.users.TryGetValue(ctx.Player.Name, out usr))
                 {
                     if (usr.chatColor != default(Color) && usr.chatColor != ChatColor.AntiqueWhite)
                         args.Color = usr.chatColor;
                     else if (usr.group.Count > 0)
                     {
-                        Group grp = GetGroup(usr.group[0]);
+                        Group grp = Permissions.GetGroup(usr.group[0]);
                         if (grp != null && grp.GroupInfo.color != default(Color) && grp.GroupInfo.color != ChatColor.AntiqueWhite)
                             args.Color = grp.GroupInfo.color;
                     }
@@ -124,7 +104,7 @@ namespace TDSMPermissions
         [Hook(HookOrder.LATE)]
         void OnPluginsLoaded(ref HookContext ctx, ref HookArgs.PluginsLoaded args)
         {
-            LoadPerms();
+            Permissions.LoadPerms(permissionsYML);
 
             if (!Server.UsingLoginSystem)
                 Login.InitSystem();
@@ -133,350 +113,15 @@ namespace TDSMPermissions
                 ProgramLog.Plugin.Log("Your Server is now protected!");
             else
                 ProgramLog.Plugin.Log("Your Server is vulnerable, Get an Authentication system!");
-        }
-        
-        public void LoadPerms()
-        {
-            Token to;
-            TextReader re = File.OpenText(permissionsYML);
+        }               
 
-            sc = new YamlScanner();
-            sc.SetSource(re);
-
-            inUsers = false;
-
-            while ((to = sc.NextToken()) != Token.EndOfStream)
-            {
-                switch (to)
-                {
-                    case Token.TextContent:
-						{
-                            switch (sc.TokenText)
-                            {
-                                case "info":
-                                    {
-                                        ProcessInfo();
-                                        break;
-                                    }
-                                case "permissions":
-                                    {
-										ProcessPermissions();
-                                        break;
-                                    }
-                                case "inheritance":
-                                    {
-                                        ParseInheritance();
-                                        break;
-                                    }
-                                case "groups":
-                                    {
-                                        if (inUsers)
-                                        {
-                                            ProcessGroups();
-                                        }
-                                        break;
-                                    }
-                                case "users":
-                                    {
-                                        inUsers = true;
-										ProcessInheritance();
-                                        break;
-                                    }
-                                default:
-                                    break;
-                            }
-                            break;
-                        }
-                    case Token.IndentSpaces:
-                        {
-                            ProcessIndent();
-                            break;
-                        }
-                    case Token.Outdent:
-                    case Token.ValueIndicator:
-                    case Token.BlockSeqNext:
-                    case Token.Comma:
-                    case Token.Escape:
-                    case Token.InconsistentIndent:
-                    case Token.Unexpected:
-                    case Token.DoubleQuote:
-                    case Token.SingleQuote:
-                    case Token.EscapedLineBreak:
-                    default:
-                        break;
-                }
-            }
-        }
-
-        private void ProcessGroups()
-        {
-            while (sc.NextToken() != Token.Outdent)
-            {
-                while (sc.NextToken() != Token.TextContent)
-				{
-                    if (sc.Token == Token.Outdent)
-                        return;
-				}
-                foreach (Group group in groups)
-                {
-                    if (group.Name == sc.TokenText)
-                    {
-                        foreach (string node in group.permissions.Keys)
-                        {
-                            bool toggle = false;
-                            group.permissions.TryGetValue(node, out toggle);
-                            if (toggle)
-                            {
-                                currentUser.hasPerm.Add(node);
-                            }
-                            else
-                            {
-                                currentUser.notHasPerm.Add(node);
-                            }
-						}
-						currentUser.group.Add(group.Name);
-                        if (!users.ContainsKey(currentUserName))
-                            users.Add(currentUserName, currentUser);
-                    }
-				}
-            }
-        }
-
-        private void ProcessIndent()
-        {
-            string tokenText = sc.TokenText;
-            if (sc.NextToken() == Token.IndentSpaces)
-                tokenText += sc.TokenText;
-            if (tokenText == "    ")
-            {
-                while (sc.NextToken() != Token.TextContent)
-                {
-                }
-                if (!inUsers)
-                {
-                    currentGroup = new Group(sc.TokenText);
-                }
-                else
-                {
-                    currentUserName = sc.TokenText;
-                    currentUser = new User();
-                }
-            }
-        }
-
-        private void ProcessInfo()
-        {
-            bool Default;
-            string Prefix;
-            string Suffix;
-            Color color = default(Color);
-            while (sc.TokenText != "default")
-            {
-                sc.NextToken();
-            }
-            while (sc.NextToken() != Token.TextContent)
-            { }
-            Default = Convert.ToBoolean(sc.TokenText);
-            if (Default)
-            {
-                defaultGroup = currentGroup.Name;
-            }
-            while (sc.TokenText != "prefix")
-            {
-                sc.NextToken();
-            }
-            while (sc.NextToken() != Token.TextContent)
-            { }
-            Prefix = sc.TokenText;
-            while (sc.TokenText != "suffix")
-            {
-                sc.NextToken();
-            }
-            while (sc.NextToken() != Token.TextContent)
-            { }
-            Suffix = sc.TokenText;
-            while (sc.TokenText != "color")
-            {
-                sc.NextToken();
-                if (sc.Token == Token.Outdent)
-				{
-					color = ChatColor.White;
-                    break;
-				}
-            }
-
-			if (sc.Token == Token.TextContent && sc.TokenText == "color")
-			{
-				while (sc.NextToken() != Token.TextContent)
-				{
-					if (sc.Token == Token.Outdent)
-						break;
-				}
-                Color col;
-                if (sc.Token == Token.TextContent && Color.TryParseRGB(sc.TokenText, out col))
-                    color = col;
-                //else
-                //    color = ChatColor.White;
-			}
-
-            if (inUsers)
-                currentUser.SetUserInfo(Prefix, Suffix, color);
-            else
-                currentGroup.SetGroupInfo(Default, Prefix, Suffix, color);
-        }
-
-        private void ParseInheritance()
-        {
-            while (sc.NextToken() != Token.TextContent)
-            {
-                if (sc.Token == Token.Outdent)
-                    return;
-            }
-            currentGroup.Inherits.Add(sc.TokenText);
-        }
-
-		private void ProcessInheritance()
-		{
-            //ProgramLog.Debug.Log("Processing group inheritances");
-			foreach (Group group in groups)
-			{
-                //ProgramLog.Debug.Log("Processing " + group.Name + "'s inheritance");
-				group.Inherits.ForEach(delegate (string s)
-				{
-                    //ProgramLog.Debug.Log("Group " + group.Name + " inherits from " + s);
-					foreach (Group groupIn in groups)
-					{
-						if (groupIn.Name == s)
-						{
-							foreach (string node in groupIn.permissions.Keys)
-							{
-								if (!group.permissions.ContainsKey(node))
-								{
-									//ProgramLog.Debug.Log("Adding node " + node + " from " + s + " to " + group.Name);
-									bool toggle = false;
-									groupIn.permissions.TryGetValue(node, out toggle);
-									group.permissions.Add(node, toggle);
-								}
-							}
-						}
-					}
-				});
-			}
-		}
-
-        private void ProcessPermissions()
-        {
-            while (sc.NextToken() != Token.Outdent)
-            {
-                while (sc.NextToken() != Token.TextContent)
-                {
-					if (sc.Token == Token.Outdent)
-					{
-						if (!inUsers)
-						{
-							 groups.Add(currentGroup);
-						}
-                        else if (!users.ContainsKey(currentUserName))
-						{
-							users.Add(currentUserName, currentUser);
-						}
-						return;
-					}
-                 }
-                bool toggle;
-                string tokenText;
-                if (sc.TokenText.Contains("-"))
-                {
-                    toggle = false;
-                    tokenText = sc.TokenText.Substring(1, sc.TokenText.Length - 1);
-                }
-                else
-                {
-                    toggle = true;
-                    tokenText = sc.TokenText;
-                }
-                if (!inUsers)
-                {
-                    if (toggle)
-                    {
-                        if (tokenText == "*")
-                        {
-							foreach (string s in Program.permissionManager.ActiveNodes)
-                            {
-                                if(!currentGroup.permissions.ContainsKey(s))
-                                    currentGroup.permissions.Add(s, toggle);
-                            }
-                        }
-                        else if (tokenText.Contains("*"))
-                        {
-                            string temp = tokenText.Remove(tokenText.Length - 2);
-							foreach (string s in Program.permissionManager.ActiveNodes)
-                            {
-                                if (s.Contains(temp))
-                                {
-                                    if (!currentGroup.permissions.ContainsKey(s))
-                                        currentGroup.permissions.Add(s, toggle);
-                                }
-                            }
-                        }
-                    }
-
-                    if (!currentGroup.permissions.ContainsKey(tokenText))
-                        currentGroup.permissions.Add(tokenText, toggle);
-                }
-                else
-                {
-                    if (toggle)
-                    {
-                        if (tokenText == "*")
-                        {
-							foreach (string s in Program.permissionManager.ActiveNodes)
-                            {
-                                currentUser.hasPerm.Add(s);
-                            }
-                        }
-						else if (tokenText.Contains("*"))
-						{
-							string temp = tokenText.Remove(tokenText.Length - 2);
-							foreach (string s in Program.permissionManager.ActiveNodes)
-							{
-								if (s.Contains(temp))
-								{
-									currentUser.hasPerm.Add(s);
-								}
-							}
-						}
-						else
-						{
-							currentUser.hasPerm.Add(tokenText);
-						}
-                    }
-                    else
-                    {
-                        currentUser.notHasPerm.Add(tokenText);
-                    }
-                }
-            }
-        }
-
-        public Group GetGroup(string Name)
-        {
-            foreach (Group grp in groups)
-            {
-                if (grp.Name == Name)
-                    return grp;
-            }
-
-            return null;
-        }
-
+        //This is what TDSM will check.
         public bool IsPermitted(string node, Player player)
         {
 			User user;
-            if (users.TryGetValue(player.Name, out user))
-            {
+            if (Permissions.users.TryGetValue(player.Name, out user))
                 return ((user.hasPerm.Contains(node) && player.AuthenticatedAs != null) || player.Op);
-            }
+
             return false;
         }
 
@@ -487,6 +132,5 @@ namespace TDSMPermissions
                 Directory.CreateDirectory(dirPath);
             }
         }
-
     }
 }
