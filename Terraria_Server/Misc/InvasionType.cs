b274c10318f9fc8ac538a4cd84a64e564940ff45
DeathCradle <DeathCradle@live.com.au>
2012-02-21 13:10:35 +1000

    Misc stuff?

---
+++ Terraria_Server/Misc/InvasionType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Terraria_Server.Misc
+{
+	public enum InvasionType : int
+	{
+		GOBLIN_ARMY = 1,
+		FROST_LEGION = 2
+	}
+}
