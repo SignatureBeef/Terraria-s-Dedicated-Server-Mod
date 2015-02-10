4bcb7225aa442665eebe022e23f40f116d025a7f
DeathCradle <DeathCradle@live.com.au>
2015-02-11 07:48:04 +1000

    Removed names

--- PluginExamples/C#/BareBones/Properties/AssemblyInfo.cs
+++ PluginExamples/C#/BareBones/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("lukesmallmon")]
+[assembly: AssemblyCopyright ("")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
