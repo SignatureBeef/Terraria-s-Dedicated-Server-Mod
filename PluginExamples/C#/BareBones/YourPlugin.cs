2504064dd58f35eaaeb8bd1448994017f3806a3e
DeathCradle <DeathCradle@live.com.au>
2015-02-25 08:37:55 +1000

    VB plugin example

---
+++ PluginExamples/C#/BareBones/YourPlugin.cs
@@ -0,0 +1,41 @@
+﻿using tdsm.api;
+using tdsm.api.Command;
+using tdsm.api.Plugin;
+
+namespace BareBones
+{
+    public class YourPlugin : BasePlugin
+    {
+        public YourPlugin()
+        {
+            this.TDSMBuild = 1;
+            this.Version = "1";
+            this.Author = "TDSM";
+            this.Name = "Simple name";
+            this.Description = "This plugin does these awesome things!";
+        }
+
+        protected override void Initialized(object state)
+        {
+            AddCommand("commandname")
+                .WithAccessLevel(AccessLevel.PLAYER)
+                .WithDescription("My command description")
+                .WithHelpText("<name>")
+                .WithHelpText("<something else> <maybe more>")
+                .WithPermissionNode("BareBones.commandname")
+                .Calls(MyCustomCommandCallback);
+        }
+
+        void MyCustomCommandCallback(ISender sender, ArgumentList args)
+        {
+            //Your implementation
+        }
+
+        [Hook(HookOrder.NORMAL)]
+        void MyFunctionNameThatDoesntMatter(ref HookContext ctx, ref HookArgs.PlayerEnteredGame args)
+        {
+            //Your implementation
+        }
+    }
+}
+
