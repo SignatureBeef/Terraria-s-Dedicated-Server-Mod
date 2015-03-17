e8f398a74a9a2fe3a49668c97dd42e69d87ee3a7
DeathCradle <DeathCradle@live.com.au>
2015-03-17 12:22:57 +1000

    WIP web permissions - files

---
+++ tdsm-core/WebInterface/Pages/Admin/Overview.cs
@@ -0,0 +1,16 @@
+﻿
+namespace tdsm.core.WebInterface.Pages.Admin
+{
+    [WebModule(Url = "/api/module/overview", Nodes = new string[]
+    {
+        "*"
+    })]
+    public class Overview : WebPage
+    {
+        public override void ProcessRequest(WebRequest request)
+        {
+            //Return server information as json.
+            //e.g. Uptime, players connected
+        }
+    }
+}
