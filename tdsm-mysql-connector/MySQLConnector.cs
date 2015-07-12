b52f28fd6f6404c578bbc2c23e96fc115e8e2ff6
DeathCradle <DeathCradle@live.com.au>
2015-07-12 15:51:06 +1000

    Started on the MySQL connector

---
+++ tdsm-mysql-connector/MySQLConnector.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using tdsm.api.Data;
+using MySql.Data.MySqlClient;
+
+namespace TDSM.Data.MySQL
+{
+    public class MySQLConnector : IDataConnector
+    {
+        static readonly MySQLQueryBuilder _builder = new MySQLQueryBuilder();
+
+        private MySqlConnection _connection;
+
+        public QueryBuilder GetBuilder()
+        {
+            return _builder;
+        }
+
+        public MySQLConnector(string connectionString)
+        {
+            _connection = new MySqlConnection();
+            _connection.ConnectionString = connectionString;
+        }
+
+        public void Open()
+        {
+            _connection.Open();
+        }
+
+        public override string ToString()
+        {
+            return "[MySQLConnector]";
+        }
+    }
+
+    public class MySQLQueryBuilder : QueryBuilder
+    {
+
+    }
+}
+
---
+++ tdsm-mysql-connector/Plugin.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using tdsm.api.Plugin;
+using tdsm.api.Data;
+using tdsm.api.Logging;
+
+namespace TDSM.Data.MySQL
+{
+    public class Plugin : BasePlugin
+    {
+        public Plugin()
+        {
+            this.TDSMBuild = 2;
+            this.Version = "1";
+            this.Author = "TDSM";
+            this.Name = "MySQL Connector";
+            this.Description = "Adds MySQL storage";
+        }
+
+        private MySQLConnector _connector;
+
+        protected override void Initialized(object state)
+        {
+            base.Initialized(state);
+        }
+
+        [Hook]
+        void OnReadConfig(ref HookContext ctx, ref HookArgs.ConfigurationLine args)
+        {
+            switch (args.Key)
+            {
+                case "mysql":
+                    if (!Storage.IsAvailable)
+                    {
+                        MySQLConnector cn = null;
+
+                        try
+                        {
+                            cn = new MySQLConnector(args.Value);
+                            cn.Open();
+                        }
+                        catch (Exception e)
+                        {
+                            ProgramLog.Error.Log("Exception connecting to MySQL database: {0}", e);
+                            return;
+                        }
+                        Storage.SetConnector(cn);
+
+                        _connector = cn;
+                    }
+                    break;
+            }
+        }
+
+        protected override void Enabled()
+        {
+            base.Enabled();
+        }
+
+        [Hook]
+        void OnStateChange(ref HookContext ctx, ref HookArgs.ServerStateChange args)
+        {
+            if (args.ServerChangeState == tdsm.api.ServerState.Initialising)
+            {
+                ProgramLog.Plugin.Log("MySQL connector is: " + (_connector == null ? "disabled" : "enabled"));
+            }
+        }
+    }
+}
+
