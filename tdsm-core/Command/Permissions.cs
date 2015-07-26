7185b2ada7099a7e2db0f10d27c22822ef6be8d0
DeathCradle <DeathCradle@live.com.au>
2015-07-26 21:36:14 +1000

    Refreshed binaries

---
+++ tdsm-core/Command/Permissions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TDSM.API.Command;
+
+namespace TDSM.Core.Command
+{
+    public partial class Entry
+    {
+        void ManagePermission(ISender sender, ArgumentList args)
+        {
+
+        }
+    }
+}
+
