00e6132c7df23aeac9d7d3df19d72981bd07061d
DeathCradle <DeathCradle@live.com.au>
2015-08-14 21:30:35 +1000

    Web server now can be ran using Libraries, and Plugins can add controllers

--- tdsm-api/Web/WebServer.cs
+++ tdsm-api/Web/WebServer.cs
@@ -4,6 +4,13 @@ using Owin;
 using System.Threading;
 using Microsoft.Owin.Hosting;
 using TDSM.API.Logging;
+using System.IO;
+using Microsoft.Owin.StaticFiles;
+using Microsoft.Owin;
+using Microsoft.Owin.FileSystems;
+using System.Web.Http.Dispatcher;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace TDSM.API
 {
@@ -11,39 +18,41 @@ namespace TDSM.API
     {
         public static HttpConfiguration Config { get; private set; }
 
-        private static readonly AutoResetEvent _switch = new AutoResetEvent(false);
+        public static string StaticFileDirectory = "Web";
+
+        internal static readonly AutoResetEvent Switch = new AutoResetEvent(false);
 
         static WebServer()
         {
             Config = new HttpConfiguration();
+
+            Config.Routes.MapHttpRoute(
+                name: "DefaultApi", 
+                routeTemplate: "api/{controller}/{id}", 
+                defaults: new { id = RouteParameter.Optional } 
+            );
+
+//            Config.DependencyResolver = new AssembliesResolver();
+            Config.Services.Replace(typeof(IAssembliesResolver), new PluginServiceResolver());
         }
 
         public static void Start(string baseAddress)
         {
-            _switch.Reset();
-            (new Thread(StartServer)).Start(baseAddress);
+            Switch.Reset();
+            (new Thread(OWINServer.StartServer)).Start(baseAddress);
         }
 
         public static void Stop()
         {
-            _switch.Set();
+            Switch.Set();
         }
+    }
 
-        private static void StartServer(object baseAddress)
+    public class PluginServiceResolver : DefaultAssembliesResolver
+    {
+        public override ICollection<Assembly> GetAssemblies()
         {
-            System.Threading.Thread.CurrentThread.Name = "Web";
-            try
-            {
-                using (WebApp.Start<OWINServer>(url: baseAddress as String))
-                {
-                    ProgramLog.Web.Log("Web server started listening on {0}", baseAddress);
-                    _switch.WaitOne();
-                } 
-            }
-            catch (Exception e)
-            {
-                ProgramLog.Log(e);
-            }
+            return AppDomain.CurrentDomain.GetAssemblies();
         }
     }
 
@@ -51,11 +60,41 @@ namespace TDSM.API
     {
         public void Configuration(IAppBuilder app)
         {
-            #if DEBUG
-            app.UseWelcomePage();
-            #endif
+//            #if DEBUG
+//            app.UseWelcomePage();
+//            #endif
             app.UseErrorPage();
             app.UseWebApi(WebServer.Config); 
+
+            app.UseFileServer(new FileServerOptions()
+                {
+                    RequestPath = new PathString("/web"),
+                    FileSystem = new PhysicalFileSystem(WebServer.StaticFileDirectory),
+                    EnableDirectoryBrowsing = false
+                });
+        }
+
+        internal static void StartServer(object baseAddress)
+        {
+            System.Threading.Thread.CurrentThread.Name = "Web";
+
+            if (!Directory.Exists(WebServer.StaticFileDirectory))
+            {
+                Directory.CreateDirectory(WebServer.StaticFileDirectory);
+            }
+
+            try
+            {
+                using (WebApp.Start<OWINServer>(url: baseAddress as String))
+                {
+                    ProgramLog.Web.Log("Web server started listening on {0}", baseAddress);
+                    WebServer.Switch.WaitOne();
+                } 
+            }
+            catch (Exception e)
+            {
+                ProgramLog.Log(e);
+            }
         }
     }
 }
---
+++ tdsm-core/WebApi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Http;
+
+namespace TDSM.Core
+{
+    public class TDSMController : ApiController
+    {
+        public System.Collections.Generic.IEnumerable<string> Get()
+        { 
+            return new string[] { "TDSM CONTROLLER" }; 
+        }
+    }
+}
+
