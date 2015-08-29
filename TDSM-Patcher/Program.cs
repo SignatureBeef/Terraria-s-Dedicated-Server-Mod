58b8b1968f530c2af083da592a0e7e9ed87c78ea
DeathCradle <DeathCradle@live.com.au>
2015-08-29 19:35:19 +1000

    Added the TDSM specific config items to the TDSM-Patcher

--- TDSM-Patcher/Program.cs
+++ TDSM-Patcher/Program.cs
@@ -62,58 +62,71 @@ namespace TDSM.Patcher
 
             OTAPatcher.CopyProjectFiles = true;
 
+            OTAPatcher.PromptToRun = false;
+
             OTAPatcher.DefaultProcess(args);
 
-//            //Post process, we can apply our additional configuration lines
-//            Console.Write("Ok\nGenerating server.config...");
-//            OTAPatcher.GenerateConfig();
+            //Post process, we can apply our additional configuration lines
+            Console.Write("Generating server.config...");
+            OTAPatcher.GenerateConfig();
 
-            var isMono = Type.GetType("Mono.Runtime") != null;
-            if (!isMono)
-            {
-                Console.Write("Ok\nUpdating icons...");
-                var res = new Vestris.ResourceLib.IconDirectoryResource(new Vestris.ResourceLib.IconFile("tdsm.ico"));
-                foreach (var fl in new string[] { OTAPatcher.OutputName + ".exe" }) //outFileMS, outFileMN })
-                {
-                    try
-                    {
-                        System.Threading.Thread.Sleep(1000);
-                        res.SaveTo(fl);
-                    }
-                    catch
-                    {
-                        Console.Write("Failed to write icon for: " + fl);
-                    }
-                }
-            
-                Console.Write("Ok\nUpdating headers...");
-                foreach (var fl in new string[] { OTAPatcher.OutputName + ".exe" })
-                {
-                    try
-                    {
-                        using (var ri = new Vestris.ResourceLib.ResourceInfo())
-                        {
-                            System.Threading.Thread.Sleep(1000);
-                            ri.Load(fl);
-            
-                            var ver = (Vestris.ResourceLib.VersionResource)ri[Vestris.ResourceLib.Kernel32.ResourceTypes.RT_VERSION].First();
-            
-                            var inf = (Vestris.ResourceLib.StringFileInfo)ver["StringFileInfo"];
-                            inf["OriginalFilename"] = OTAPatcher.OutputName + ".exe" + '\0';
-                            inf["InternalName"] = OTAPatcher.OutputName + ".exe" + '\0';
-                            inf["ProductName"] = OTAPatcher.OutputName + '\0';
-                            inf["FileDescription"] = OTAPatcher.OutputName + '\0';
-            
-                            ri.Unload();
-                            ver.SaveTo(fl);
-                        }
-                    }
-                    catch
-                    {
-                        Console.WriteLine("Failed to write header for: " + fl);
-                    }
-                }
-            }
+            Console.Write("Ok\nUpdating binaries...");
+            OTAPatcher.BinariesFiles.Add(Path.Combine("Plugins", "TDSM.Core.dll"));
+            OTAPatcher.BinariesFiles.Add(Path.Combine("Plugins", "TDSM.Core.dll.mdb"));
+            OTAPatcher.BinariesFiles.Add(Path.Combine("Plugins", "TDSM.Core.pdb"));
+            Console.Write("Ok\n");
+
+            //Update dev files
+            OTAPatcher.UpdateBinaries();
+
+            OTAPatcher.PromptUserToRun(OTAPatcher.OutputName + ".exe", Type.GetType("Mono.Runtime") != null);
+
+//            var isMono = Type.GetType("Mono.Runtime") != null;
+//            if (!isMono)
+//            {
+//                Console.Write("Ok\nUpdating icons...");
+//                var res = new Vestris.ResourceLib.IconDirectoryResource(new Vestris.ResourceLib.IconFile("tdsm.ico"));
+//                foreach (var fl in new string[] { OTAPatcher.OutputName + ".exe" }) //outFileMS, outFileMN })
+//                {
+//                    try
+//                    {
+//                        System.Threading.Thread.Sleep(1000);
+//                        res.SaveTo(fl);
+//                    }
+//                    catch
+//                    {
+//                        Console.Write("Failed to write icon for: " + fl);
+//                    }
+//                }
+//            
+//                Console.Write("Ok\nUpdating headers...");
+//                foreach (var fl in new string[] { OTAPatcher.OutputName + ".exe" })
+//                {
+//                    try
+//                    {
+//                        using (var ri = new Vestris.ResourceLib.ResourceInfo())
+//                        {
+//                            System.Threading.Thread.Sleep(1000);
+//                            ri.Load(fl);
+//            
+//                            var ver = (Vestris.ResourceLib.VersionResource)ri[Vestris.ResourceLib.Kernel32.ResourceTypes.RT_VERSION].First();
+//            
+//                            var inf = (Vestris.ResourceLib.StringFileInfo)ver["StringFileInfo"];
+//                            inf["OriginalFilename"] = OTAPatcher.OutputName + ".exe" + '\0';
+//                            inf["InternalName"] = OTAPatcher.OutputName + ".exe" + '\0';
+//                            inf["ProductName"] = OTAPatcher.OutputName + '\0';
+//                            inf["FileDescription"] = OTAPatcher.OutputName + '\0';
+//            
+//                            ri.Unload();
+//                            ver.SaveTo(fl);
+//                        }
+//                    }
+//                    catch
+//                    {
+//                        Console.WriteLine("Failed to write header for: " + fl);
+//                    }
+//                }
+//            }
         }
     }
 }
